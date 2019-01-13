ba7749d34824d8bef6f973c8a7fab82cb17a77e4
junior-16 <junior.ramisch@gmail.com>
2019-01-13 02:07:11 -0200

    Data Binding and Event Binding

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//MetaData
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Task-Manager';
+}
